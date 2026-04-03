01478ee9fe75781b46839ce7f88b1561f93060c6
M-Anonymous <m_anonymous@163.com>
2026-04-03 14:29:05 +0800

    fix

--- mcp/mcp_client/src/main/java/com/study/ai/controller/McpController.java
+++ mcp/mcp_client/src/main/java/com/study/ai/controller/McpController.java
@@ -2,24 +2,26 @@ package com.study.ai.controller;
 
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class McpController {
 
     private final ChatClient chatClient;
 
-    public McpController(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+    public McpController(ChatClient.Builder builder, ToolCallbackProvider toolCallbackProvider) {
+        this.chatClient = builder.defaultToolCallbacks(toolCallbackProvider).build();
     }
 
     @GetMapping("/ask")
-    public String ask(@RequestParam String question){
+    public Flux<String> ask(@RequestParam String question){
         return chatClient.prompt()
                 .user(question)
-                .call()
+                .stream()
                 .content();
     }
 }
